6e1bdca755d2905c9ba3520068c91483c4dcd622
ThibaultKlein <thibault@prolificinteractive.com>
2017-04-27 16:32:46 -0400

    Initial commit

---
+++ Kumi/Kumi.h
@@ -0,0 +1,19 @@
+//
+//  Kumi.h
+//  Kumi
+//
+//  Created by Thibault Klein on 4/27/17.
+//  Copyright © 2017 Prolific Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kumi.
+FOUNDATION_EXPORT double KumiVersionNumber;
+
+//! Project version string for Kumi.
+FOUNDATION_EXPORT const unsigned char KumiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kumi/PublicHeader.h>
+
+
