d77d450ad44fecef8ba38b8605ababd66271bd62
ThibaultKlein <thibault@prolificinteractive.com>
2017-10-20 15:07:39 -0400

    Updated Kumi for Swift 4

--- Kumi.podspec
+++ Kumi.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "Kumi"
-  s.version          = "0.0.4"
+  s.version          = "0.0.5"
   s.summary          = "An iOS framework for easy theme integration to your application"
 
   s.description      = <<-DESC
--- KumiTests/Shadow/ShadowStyleTests.swift
+++ KumiTests/Shadow/ShadowStyleTests.swift
@@ -36,8 +36,7 @@ class ShadowStyleTests: XCTestCase {
         XCTAssertEqual(shadowStyle.shadowRadius, 14)
         XCTAssertEqual(shadowStyle.shadowOffset.width, 0.0)
         XCTAssertEqual(shadowStyle.shadowOffset.height, 7.0)
-        XCTAssertEqual(shadowStyle.shadowColor, UIColor(colorLiteralRed: 1, green: 0, blue: 0, alpha: 1).cgColor)
-
+        XCTAssertEqual(shadowStyle.shadowColor, UIColor(red: 1, green: 0, blue: 0, alpha: 1).cgColor)
     }
 
 }
