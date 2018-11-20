f0025005d812d1132223747dac97c2e9046d6767
Nattawut Singhchai <wut@prolificinteractive.com>
2018-11-20 14:15:13 +0700

    Update class interface

--- Kumi/Core/Kumi.swift
+++ Kumi/Core/Kumi.swift
@@ -14,39 +14,39 @@ public struct Kumi {
     // private section
     
     /// Color theme object.
-    fileprivate static var _color: ColorTheme?
+    fileprivate static var _color = ColorTheme(json: JSON({}))
     
     /// Font theme object.
-    fileprivate static var _font: FontTheme?
+    fileprivate static var _font = FontTheme(json: JSON({}))
     
     /// Layer theme object.
-    fileprivate static var _layer: LayerTheme?
+    fileprivate static var _layer = LayerTheme(json: JSON({}))
     
     /// Animation theme object.
-    fileprivate static var _animation: AnimationTheme?
+    fileprivate static var _animation = AnimationTheme(json: JSON({}))
     
     /// Shadow theme object.
-    fileprivate static var _shadow: ShadowTheme?
+    fileprivate static var _shadow = ShadowTheme(json: JSON({}))
     
     // public
     
-    public static var color: ColorTheme? {
+    public static var color: ColorTheme {
         return _color
     }
     
-    public static var font: FontTheme? {
+    public static var font: FontTheme {
         return _font
     }
     
-    public static var layer: LayerTheme? {
+    public static var layer: LayerTheme {
         return _layer
     }
     
-    public static var animation: AnimationTheme? {
+    public static var animation: AnimationTheme {
         return _animation
     }
     
-    public static var shadow: ShadowTheme? {
+    public static var shadow: ShadowTheme {
         return _shadow
     }
     
@@ -56,8 +56,15 @@ public struct Kumi {
     }
     
     public static func setup(bundle: Bundle) {
-        let files = bundle.paths(forResourcesOfType: "json", inDirectory: nil)
-        let jsons = files.compactMap { JSONHelper.readJSON(path: $0) }.filter { !$0["kumi"].isEmpty }
+        setup(withFilePaths: bundle.paths(forResourcesOfType: "json", inDirectory: nil))
+    }
+    
+    public static func setup(withFilePaths paths: [String]) {
+        setup(withJSONs: paths.compactMap { JSONHelper.readJSON(path: $0) }.filter { !$0["kumi"].isEmpty })
+    }
+    
+    @available(*, deprecated, message: "This is old json pattern")
+    public static func setup(withJSONs jsons: [JSON]) {
         for json in jsons {
             switch json["kumi"]["type"].stringValue {
             case "color":
@@ -78,5 +85,14 @@ public struct Kumi {
             default: break
             }
         }
+    }
+    
+    public static func setup(withJSON json: JSON) {
+        let j = json["json"]
+        _color = ColorTheme(json: j["color"])
+        _font = FontTheme(json: j["text"])
+        _layer = LayerTheme(json: j["layer"])
+        _animation = AnimationTheme(json: j["animation"])
+        _shadow = ShadowTheme(json: j["shadow"])
     }
 }
