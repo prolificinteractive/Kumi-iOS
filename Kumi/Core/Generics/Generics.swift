50ede5185900f9431516d4953fa945a3309385a5
Nattawut Singhchai <wut@prolificinteractive.com>
2018-11-28 04:26:53 +0700

    Make generics able to subscript

--- Kumi/Core/Generics/Generics.swift
+++ Kumi/Core/Generics/Generics.swift
@@ -10,15 +10,22 @@ import SwiftyJSON
 
 public final class Generics {
     
+    private var json: JSON
+    
     public var colors: ColorTheme!
     public var texts: FontTheme!
     public var layers: LayerTheme!
     public var animations: AnimationTheme!
     
     init(json: JSON) {
+        self.json = json
         colors = ColorTheme(json: json["colors"])
         texts = FontTheme(json: json["texts"])
         layers = LayerTheme(json: json["layers"])
         animations = AnimationTheme(json: ["animations"])
+    }
+    
+    public subscript(path: JSONSubscriptType) -> JSON {
+        return json[path]
     }
 }
