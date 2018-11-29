0ed1070a3fb7befcfcc1e3808ffca081484b3065
Nattawut Singhchai <wut@prolificinteractive.com>
2018-11-30 05:28:30 +0700

    Animation tests

--- KumiTests/Theme/ThemeTests.swift
+++ KumiTests/Theme/ThemeTests.swift
@@ -28,6 +28,8 @@ class ThemeTests: XCTestCase {
         XCTAssertEqual(Kumi.generics.texts.bodyNormalTextStyle.regular.textColor!.toHexString(), "#ff4f0421")
         XCTAssertEqual(Kumi.generics.texts.displayNormalTextStyle.regular.font.pointSize, 32)
         XCTAssertEqual(Kumi.functionals["NeonLabel"]["isHighlighted"]["animationStyle"].uiViewAnimationStyle.duration, 4)
+        XCTAssertEqual(Kumi.generics.animations.extraFastUIViewAnimationStyle.duration, 0.17499999999999999)
+        XCTAssertEqual(Kumi.generics.animations.extraFastCABasicAnimationStyle.duration, 0.087499999999999994)
     }
 
 }
