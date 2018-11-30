7aea7c9daf7ef90a69f0761cfc6cc614385ddb9b
Nattawut Singhchai <wut@prolificinteractive.com>
2018-12-01 05:17:46 +0700

    Fix CAMediaTimingFunction+JSON

--- Kumi/Core/Utility/CAMediaTimingFunction+JSON.swift
+++ Kumi/Core/Utility/CAMediaTimingFunction+JSON.swift
@@ -11,10 +11,10 @@ import SwiftyJSON
 extension CAMediaTimingFunction {
     
     convenience init?(json: JSON) {
-        guard let c1x = json["c1x"].float,
-            let c1y = json["c1y"].float,
-            let c2x = json["c2x"].float,
-            let c2y = json["c2y"].float else {
+        guard let c1x = json["c1x"].kumiValue.float,
+            let c1y = json["c1y"].kumiValue.float,
+            let c2x = json["c2x"].kumiValue.float,
+            let c2y = json["c2y"].kumiValue.float else {
                 return nil
         }
 
